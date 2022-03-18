edc676c9b4b38acab0e54431c6b6141b44436872
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-03-18 16:04:58 -0500

    Update hierarchical_agglomerative_clustering_analysis.py

--- hierarchical_agglomerative_clustering_analysis.py
+++ hierarchical_agglomerative_clustering_analysis.py
@@ -10,6 +10,18 @@ import scipy.cluster.hierarchy as hierarchy
 import sklearn.metrics as metrics
 from strip_headers_and_footers import strip_headers
 
+class Delta:
+    def fit(self, X, y):
+        self.train_y = np.array(y)
+        self.scaler = preprocessing.StandardScaler(with_mean=False)
+        self.train_X = self.scaler.fit_transform(X)
+        return self
+    
+    def predict(self, X, metric='cityblock'):
+        X = self.scaler.transform(X)
+        dists = scidist.cdist(X, self.train_X, metric=metric)
+        return self.train_y[np.argmin(dists, axis=1)]
+
 def load_directory(directory, max_length):
     documents, authors, titles = [], [], []
     for filename in os.scandir(directory):
@@ -36,20 +48,37 @@ def load_directory(directory, max_length):
     return documents, authors, titles
     
 directory = "./training_data/"
+test_directory = "./testing_data/"
 
+with open('./testing_data/j_mate_of_the_daylight.txt') as f:
+    data = f.read()
+    data = data.lower().split()
+
+print(len(data))
+    
 documents, authors, titles = load_directory(directory, 20000)
+test_documents, test_authors, test_titles = load_directory(test_directory, 7753)
 
-vectorizer = text.CountVectorizer(max_features=30, token_pattern=r"(?u)\b\w\w+\b")
+print(test_titles)
+vectorizer = text.CountVectorizer(token_pattern=r"(?u)\b\w\w+\b")
 v_documents = vectorizer.fit_transform(documents).toarray()
 v_documents = preprocessing.normalize(v_documents.astype(float), norm='l1')
 scaler = preprocessing.StandardScaler()
-s_documents = scaler.fit_transform(v_documents)
+v_documents = scaler.fit_transform(v_documents)
 
 print(f'N={v_documents.shape[0]} documents with '
       f'V={v_documents.shape[1]} features.')
+
+v_test_docs = vectorizer.transform(test_documents)
+v_test_docs = preprocessing.normalize(v_test_docs.astype(float), norm='l1')
+v_test_docs = scaler.transform(v_test_docs.toarray())
       
 dm = scidist.pdist(v_documents, 'cityblock')
 linkage_object = hierarchy.linkage(dm, method='complete')
+
+all_documents = np.vstack((v_documents, v_test_docs))
+all_dm = scidist.pdist(all_documents,'cityblock')
+linkage_object_all = hierarchy.linkage(all_dm, method='complete')
 
 def plot_tree(linkage_object, labels, figsize=(15, 15), ax=None):
     if ax is None:
@@ -68,4 +97,5 @@ def plot_tree(linkage_object, labels, figsize=(15, 15), ax=None):
         s.set_visible(False)
     plt.show()
 
-plot_tree(linkage_object, titles)
+#plot_tree(linkage_object, titles)
+plot_tree(linkage_object_all, authors + test_titles)
