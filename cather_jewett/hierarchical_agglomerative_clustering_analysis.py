284880f5e421830a769d8a81644702e0b9330e17
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-06-09 15:00:02 -0500

    updates

--- cather_jewett/hierarchical_agglomerative_clustering_analysis.py
+++ cather_jewett/hierarchical_agglomerative_clustering_analysis.py
@@ -100,16 +100,16 @@ def plot_tree(linkage_object, labels, figsize=(15, 15), ax=None):
 #plot_tree(linkage_object, titles)
 plot_tree(linkage_object_all, titles + test_title)
 
-with open(r'C:\Users\KSpicer\Documents\GitHub\cather_jewett_comparisons\testing_data\c_archbishop.txt', encoding='utf-8') as f:
-    data = f.read()
-    data = data.lower().split()
+#@with open(r'C:\Users\KSpicer\Documents\GitHub\cather_jewett_comparisons\testing_data\c_archbishop.txt', encoding='utf-8') as f:
+    #data = f.read()
+    #data = data.lower().split()
     
-with open(r'C:\Users\KSpicer\Documents\GitHub\cather_jewett_comparisons\training_data\c_my_antonia.txt', encoding='utf-8') as f:
-    antonia = f.read()
-    antonia = antonia.lower().split()
+#with open(r'C:\Users\KSpicer\Documents\GitHub\cather_jewett_comparisons\training_data\c_my_antonia.txt', encoding='utf-8') as f:
+    #antonia = f.read()
+    #antonia = antonia.lower().split()
 
-with open(r'C:\Users\KSpicer\Documents\GitHub\cather_jewett_comparisons\training_data\c_o_pioneers.txt', encoding='utf-8') as f:
-    pioneers = f.read()
-    pioneers = pioneers.lower().split()
+#with open(r'C:\Users\KSpicer\Documents\GitHub\cather_jewett_comparisons\training_data\c_o_pioneers.txt', encoding='utf-8') as f:
+    #pioneers = f.read()
+    #pioneers = pioneers.lower().split()
     
-print(len(data), len(antonia), len(pioneers))
+#print(len(data), len(antonia), len(pioneers))
